d05c5cd2f535a85d2cd4aea75768750f7fd77a91
Luis Eguia <jlewicho@gmail.com>
2018-05-20 23:54:33 -0500

    Pruebas correos

--- app/Http/Controllers/HistoriaController.php
+++ app/Http/Controllers/HistoriaController.php
@@ -27,7 +27,7 @@ class HistoriaController extends Controller
 				}
 
 				$sprint = Sprint::where('proyecto_id', Session::get('proyecto_id'))->where('estatus', 'activo');
-				dd($sprint);
+				// dd($sprint);
 				if (count($sprint) > 0) {
 					$historias = $sprint->first()->historias->where('estatus', '!=', 'baja');
 				}
